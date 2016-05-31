645c1d56e6e72f923526f0ad445e7f380ce7f41d
p.briones <p.briones@tcs.com>
2016-05-31 12:42:53 -0400

    fixed incorrect function scope in dialog

--- test/dialog.spec.js
+++ test/dialog.spec.js
@@ -1,1 +1,31 @@
 
+'use strict';
+var expect = require('chai').expect;
+var Dialog = require('../lib/dialog');
+var dialog = new Dialog({
+  username: 'c2d4e7c8-5dfb-41fe-b617-da52e9ab8bd5',
+  password: 'TKmv3QkOx8ek'
+});
+describe('dialog', function(){
+  it('.recognize', function(){
+    expect(dialog.recognize).to.be.a('function');
+  })
+  it('.createDialog', function(){
+    expect(dialog.createDialog).to.be.a('function');
+  })
+  it('.deleteDialog', function(){
+    expect(dialog.deleteDialog).to.be.a('function');
+  })
+  it('.updateDialog', function(){
+    expect(dialog.updateDialog).to.be.a('function');
+  })
+  it('.conversation', function(){
+    expect(dialog.conversation).to.be.a('function');
+  })
+  it('.getConversation', function(){
+    expect(dialog.getConversation).to.be.a('function');
+  })
+  it('.getProfile', function(){
+    expect(dialog.getProfile).to.be.a('function');
+  })
+})
